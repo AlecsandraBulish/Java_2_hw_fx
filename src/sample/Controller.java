f18c6cb8ec4a11fb6e61d815fd51b93ec99d83ee
AlexBulish <alecsandra-bulish>
2021-02-11 16:00:44 +0300

    create startWindow

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,24 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class Controller {
+    @FXML
+    public TextArea areaCenter;
+    @FXML
+    public Button send;
+    @FXML
+    public TextField textField;
 
+    @FXML
+    public void clickSend(ActionEvent actionEvent) {
+        areaCenter.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+
+    }
 }
